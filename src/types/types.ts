daa722945fae818399cd5a3d22a61df5e16ffb0b
dusanMilenkovic989 <dusan.milenkovic989@gmail.com>
2024-01-28 16:06:59 +0100

    Add top level comment and minor export type change to types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,10 @@
 import { RequestHandler } from 'express'
 import { HTTP_METHODS } from './'
 
+/*
+    Using one file for exporting the application types over the 'namespace' convention is a matter of personal choice.
+*/
+
 /**
  * Describes property descriptor type with customized *value* property to represent the Express RequestHandler type and, hence, prevent method decorators to be used on methods other than the Express request (route) handler.
  */
@@ -15,4 +19,4 @@ interface HandlerPropertyDescriptor extends PropertyDescriptor
 type HTTPMethodValue = keyof typeof HTTP_METHODS
 
 
-export { HandlerPropertyDescriptor, HTTPMethodValue }
+export type { HandlerPropertyDescriptor, HTTPMethodValue }
